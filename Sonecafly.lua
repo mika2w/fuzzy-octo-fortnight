009cef136803211e90272db37146ceb04792a4f4
mika2w <114830048+mika2w@users.noreply.github.com>
2025-05-08 01:10:38 -0300

    Sonecafly.lua

--- Sonecafly.lua
+++ Sonecafly.lua
@@ -1,8 +1,17 @@
-local UILibrary = require(game.ReplicatedStorage.UILibrary.MainModule)
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+player = game.Players.LocalPlayer
+char = player.Character
 
-local screenGui = Instance.new("ScreenGui", game.Players.LocalPlayer:WaitForChild("PlayerGui"))
-local myButton = UILibrary.Components.Button.new("Clique Aqui", screenGui)
-
-myButton.MouseButton1Click:Connect(function()
-    print("Botão clicado!")
-end)
+for i = 1, 500  do
+	part = Instance.new("Part", char)
+	part.Size = Vector3.zero
+	weld = Instance.new("Weld", part)
+	weld.Part1 = char.HumanoidRootPart
+	weld.Part0 = part
+	part.CanCollide = false
+	part.Size = Vector3.new(6,6,6) -- Max size so the player doesnt freeze server sided
+	part.CustomPhysicalProperties = PhysicalProperties.new(100, 0, 0, 1, 10)
+	part.Transparency = 1
+end
